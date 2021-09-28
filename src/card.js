0c10bb6ba9c73464f0107b0f3c50393dca517c4c
chamikarajanapriya <chamikarajanapriya@yahoo.com>
2021-09-28 19:46:57 +0530

    add CSS styles to the Card Component

--- src/card.js
+++ src/card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = () => {
 	return(
-		<div calssName ='bg-light-green dib br3 pa3 ma2 grow'>
+		<div calssName ='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
 			<img alt='Photo' src= 'https://robohash.org/test?200*200' />
 			<div>
 				<h2> Jone Doe </h2>
