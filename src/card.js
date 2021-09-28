e3393bc4e84ab57beb579c4b6fb91720d11f74de
chamikarajanapriya <chamikarajanapriya@yahoo.com>
2021-09-28 20:03:04 +0530

    add the id to the card componet for generate different robo images.this is done by using template strings

--- src/card.js
+++ src/card.js
@@ -4,7 +4,7 @@ import 'tachyons';
 const Card = (props) => {
 	return(
 		<div calssName ='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='Photo' src= 'https://robohash.org/test?200*200' />
+			<img alt='Photo' src= {`https://robohash.org/${props.id}?200*200`} />
 			<div>
 				<h2> {props.name} </h2>
 				<p> {props.email} </p>
