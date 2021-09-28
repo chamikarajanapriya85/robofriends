fa102e597749d283b534df1791a9d1a21e69395e
chamikarajanapriya <chamikarajanapriya@yahoo.com>
2021-09-28 19:13:32 +0530

    create basic card component

---
+++ src/card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+	return(
+		<div>
+			<img alt='Photo' src= '' />
+			<div>
+				<h2> Jone Doe </h2>
+				<p> Jone.doe@gamil.com </p>
+			</div>
+		</div>
+	);
+}
+
+export default Card;
