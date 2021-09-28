cfe52e0df628d2fbef33a5c50db802cae8f262a5
chamikarajanapriya <chamikarajanapriya@yahoo.com>
2021-09-28 20:07:40 +0530

    destructuring further

--- src/card.js
+++ src/card.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import 'tachyons';
 
-const Card = (props) => {
-	const {id,name,email} = props;
+const Card = ({id,name,email}) => {
+	
 	return(
 		<div calssName ='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
 			<img alt='Photo' src= {`https://robohash.org/${id}?200*200`} />
