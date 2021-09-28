fbf26e0a07fa76eac2453212e6f63e58f68d81fe
chamikarajanapriya <chamikarajanapriya@yahoo.com>
2021-09-28 19:29:31 +0530

    create the basic card component

--- src/card.js
+++ src/card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = () => {
 	return(
 		<div>
-			<img alt='Photo' src= '' />
+			<img alt='Photo' src= 'https://robohash.org/test?200*200' />
 			<div>
 				<h2> Jone Doe </h2>
 				<p> Jone.doe@gamil.com </p>
