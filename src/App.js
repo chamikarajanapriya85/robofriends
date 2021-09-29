94a3225cd2565f3cd732fc20738f2122e5ee850a
chamikarajanapriya <chamikarajanapriya@yahoo.com>
2021-09-29 13:54:03 +0530

    add State to the App componennt and pass the state to its children as props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React,{Component} from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
 import SearchBox from './SearchBox';
 import 'tachyons';
 
-const App = () => {
-	return(
+
+class App extends React.Component{
+	constructor(){
+		super()
+		this.state={
+			robots:robots,
+			searchfield:''
+		}
+	}
+	onSearchChange=(event)=>{
+		this.setState({searchfield:event.target.value})
+		//console.log(event.target.value);
+		
+		})
+	}
+	render(){
+		const filterRobots = this.state.robots.filter(robots => {
+			return robots.name.toLowerCase().includes(this.state.serachfield.toLowerCase())
+	  return(
 		<div className ='tc'>
 			<h1> Robo Friends </h1>
-			<SearchBox />
-			<CardList robots={robots} />
+			<SearchBox searchChange ={this.onSearchChange} />
+			<CardList robots={filterRobots} /> //insted of this.state.robots
 
 		</div>
-	);
+	);	
+	}
 }
+// const App = () => {
+// 	return(
+// 		<div className ='tc'>
+// 			<h1> Robo Friends </h1>
+// 			<SearchBox />
+// 			<CardList robots={robots} />
+
+// 		</div>
+// 	);
+// }
 export default App;
