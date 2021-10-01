4a75168355d7cd1be0a5b764f949be8bc6c1277b
chamikarajanapriya <chamikarajanapriya@yahoo.com>
2021-10-01 15:33:42 +0530

    adds the Scroll component to the project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import 'tachyons';
 import './App.css';
 
@@ -32,14 +33,16 @@ class App extends React.Component{
 			return robots.name.toLowerCase().includes(this.state.serachfield.toLowerCase()
 		})
 		//console.log('render');
-	  if (robots.length === 0){
+	  if (this.state.robots.length === 0){
 	  	return <h1> Loading </h1>;
 	  }else{
 	  	return(
 			<div className ='tc'>
 				<h1 className = 'f2'> Robo Friends </h1>
 				<SearchBox searchChange ={this.onSearchChange} />
-				<CardList robots={filterRobots} /> //insted of this.state.robots
+				<Scroll> 
+					<CardList robots={filterRobots} /> //insted of this.state.robots
+				</Scroll>
 			</div>
 	 	);	
 	  }
